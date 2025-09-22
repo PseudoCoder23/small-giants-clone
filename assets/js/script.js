5504f17e240902ac3412975ba8675a0c96c596f0
PseudoCoder23 <giorgioragusa23@gmail.com>
2025-09-22 11:40:16 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+/*
+ JS to toggle scroll axis styles
+*/
+/* const control = document.getElementById("direction-toggle");
+const marquees = document.querySelectorAll(".marquee");
+const wrapper = document.querySelector(".wrapper"); */
